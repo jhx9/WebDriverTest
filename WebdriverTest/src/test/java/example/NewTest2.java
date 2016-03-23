f4837c1800249b07cb3bfaaa91cf946ec5727c34
jhx9 <jimmy.jhhuang@mic.com.tw>
2016-03-23 18:13:56 +0800

    ewqe

--- WebdriverTest/src/test/java/example/NewTest2.java
+++ WebdriverTest/src/test/java/example/NewTest2.java
@@ -1,33 +1,23 @@
 package example;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
  
 public class NewTest2 {
-private static FirefoxDriver driver;
-WebElement element;
- 
-@BeforeClass
-public static void openBrowser(){
-    driver = new FirefoxDriver();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	@Test(invocationCount = 2)
+	public void loadTestThisWebsite() {
+
+		WebDriver driver = new FirefoxDriver();		
+		driver.get("http://www.google.com");
+		System.out.println("Page Title is " + driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
+
+	}
 }
- 
-@Test
-public void testWebsite(){
-    System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-    driver.get("http://www.5balloons.info");
-    Assert.assertEquals("5 Balloons | Jack of many web trades", driver.getTitle());
-    System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
-}
- 
-@AfterClass
-public static void closeBrowser(){
-    driver.quit();
-}
-}
+
